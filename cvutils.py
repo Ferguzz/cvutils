c1b01b8b5358bf41a1acb3c2dff0c42d59b65487
Tom Myers <tom.stephen.myers@gmail.com>
2012-03-03 19:04:02 +0000

    First commit.  Outline of some functions to come.

---
+++ cvutils.py
@@ -0,0 +1,22 @@
+import cv
+
+def rotate(im, angle):
+	pass
+	
+def contrast(im, value):
+	pass
+
+def normalise(im):
+	pass
+
+def resize(im, size):
+	pass
+
+def blackandwhite(im):
+	pass
+
+def saltandpepper(im, level):
+	pass
+
+def gaussiannoise(im, level):
+	pass
